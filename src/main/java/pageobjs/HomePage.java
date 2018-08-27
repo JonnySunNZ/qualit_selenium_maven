7400b79ae3b24d9391806f3ab2880319adfd8cde
JonnySunNZ <yi.sun@outlook.co.nz>
2018-08-27 12:44:53 +1200

    Add files via upload

--- src/main/java/pageobjs/HomePage.java
+++ src/main/java/pageobjs/HomePage.java
@@ -5,24 +5,24 @@ import org.openqa.selenium.WebDriver;
 import common.BasePage;
 
 public class HomePage extends BasePage {
-	private String searchButtonLoc = "/html/body/div[1]/div/a[2]";
-	private String inputFieldLoc = "/html/body/div[1]/div/div[2]/form/fieldset/div/div/input";
-	private String goBtn = "/html/body/div[1]/div/div[2]/form/fieldset/input";
+	private String searchButtonLoc = "#search";
+	private String inputFieldLoc = "#SearchForm_SearchForm_Search";
+	private String goBtn = "#SearchForm_SearchForm_action_results";
 
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
 
 	public void clickSearchBtn() {
-		findByXPath(searchButtonLoc).click();
+		findByCss(searchButtonLoc).click();
 	}
 	
 	public void inputSearchContent() {
-		findByXPath(inputFieldLoc).sendKeys("Meet our team");
+		findByCss(inputFieldLoc).sendKeys("Meet our team");
 	}
 	
 	public void clickGoBtn() {
-		findByXPath(goBtn).click();
+		findByCss(goBtn).click();
 	}
 	
 }
